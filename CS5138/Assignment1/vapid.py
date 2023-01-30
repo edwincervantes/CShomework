7d449346e4469cdfc2eafcc3d1a66a6812a3f493
EJ Cervantes <edwin.j.cerv@gmail.com>
2023-01-30 14:34:47 -0500

    Update vapid.py
    adding stuff to do for tn

--- CS5138/Assignment1/vapid.py
+++ CS5138/Assignment1/vapid.py
@@ -1,6 +1,10 @@
 # Edwin Cervantes
 # Assignment 1 - Vapid
 
+# Accept 2 args (Path and target Virtual addy(needs to accept both hexadecimal designated by 0x and all lowercase letters and decimal form))
+# If path or VA don't exist, print an error message and exit with Status Code of 1
+# If the target virtual address cannot be found in one of the 32-bit PE file's sections, vapid will print 0x97ad63 -> ??
+
 if __name__ == "__main__":
     print("Hello buns")
 
