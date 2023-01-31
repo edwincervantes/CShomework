23c6cecc7448392930e21ec24d6e62661dcbed91
Edwin <edwin.j.cerv@gmail.com>
2023-01-30 19:58:18 -0500

    getting formatting

--- CS5138/Assignment1/vapid.py
+++ CS5138/Assignment1/vapid.py
@@ -1,12 +1,55 @@
+#!/bin/python3
 # Edwin Cervantes
 # Assignment 1 - Vapid
+# Run PyLint before turning in
+import sys
+import os
 
-# Accept 2 args (Path and target Virtual addy(needs to accept both hexadecimal designated by 0x and all lowercase letters and decimal form))
-# If path or VA don't exist, print an error message and exit with Status Code of 1
-# If the target virtual address cannot be found in one of the 32-bit PE file's sections, vapid will print 0x97ad63 -> ??
+
+def is_hex(s):
+    try:
+        int(s, 16)
+        return True
+    except ValueError:
+        return False
+
+
+def valid_args(vap_args):
+    flag = True
+    # We use 3 because vapid is counted as an arg during execution
+    if len(vap_args) != 3:
+        print("Incorrect number of arguments. Expected 2, and got {}".format(len(vap_args) - 1))
+        flag = False
+    if not os.path.exists(vap_args[1]):
+        # Maybe we should check if it is an exe here or when we get the header info
+        print("{} is not a valid path".format(vap_args[1]))
+        flag = False
+    # Check if hex then check if decimal
+    if not is_hex(vap_args[2][2:]):
+        if not int(vap_args[2]):
+            print("The Target Virtual Address must be in hexadecimal form (0x1234) or decimal")
+            flag = False
+    return flag
+
+
+def check_tva(tva):
+    valid_tva = True
+
+    return valid_tva
+
 
 if __name__ == "__main__":
-    print("Hello buns")
+    # Get the total number of args passed to the demo.py
+    vapid_args = list(sys.argv)
+    if not valid_args(vapid_args):
+        exit(1)
+    exe_path = vapid_args[1]
+    target_virtual_addr = vapid_args[2]
+    if not check_tva(target_virtual_addr):
+        print("{} -> ???".format(target_virtual_addr))
+
+    # Get the arguments list
 
 else:
     print("Whoops")
+    exit(1)
