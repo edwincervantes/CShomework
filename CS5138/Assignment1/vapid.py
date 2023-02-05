beef1c28a8052175854b4c335d62b64d5400bdf8
Edwin <edwin.j.cerv@gmail.com>
2023-02-04 23:06:28 -0500

    vapid changes

--- CS5138/Assignment1/vapid.py
+++ CS5138/Assignment1/vapid.py
@@ -17,7 +17,7 @@ def is_hex(s):
 
 
 def valid_args(vap_args):
-    flag = True
+    flag = False
     # We use 3 because vapid is counted as an arg during execution
     if len(vap_args) != 3:
         print("Incorrect number of arguments. Expected 2, and got {}".format(len(vap_args) - 1))
@@ -26,18 +26,16 @@ def valid_args(vap_args):
         print("{} is not a valid path".format(vap_args[1]))
         flag = False
     # Check if hex then check if decimal
-    if not is_hex(vap_args[2][2:]):
-        print("Not a valid hexidecimal, checking for decimal...")
-        if not int(vap_args[2]):
-            print("The Target Virtual Address must be in hexadecimal form (0x1234) or decimal")
-            flag = False    
+    if (vap_args[2][:2] == '0x'):
+ 	   if is_hex(vap_args[2][2:]):
+ 	   	flag = True
+
+    elif int(vap_args[2]):
+        flag = True  
+          
+    if flag is False:
+    	        print("The Target Virtual Address must be in hexadecimal form (0x1234) or decimal")
     return flag
-
-
-def check_tva(tva):
-    valid_tva = True
-
-    return valid_tva
 
 
 def check_32bit(pe):
@@ -45,10 +43,6 @@ def check_32bit(pe):
     if not hex(pe.FILE_HEADER.Machine) == '0x14c':
         bits = False
     return bits
-
-
-def get_entry_point(pe):
-    return hex(pe.OPTIONAL_HEADER.AddressOfEntryPoint)
 
 
 def get_image_base(pe):
@@ -77,7 +71,7 @@ def get_target_section(sections, target_virtual_addr):
         if int(target_virtual_addr, 16) > int(curr, 16):
             target_section = key
     if target_section == '':
-        print('{} --> ???'.format(target_virtual_addr))
+        print('{} -> ???'.format(target_virtual_addr))
         exit(1)
     else:
         return target_section
@@ -90,32 +84,41 @@ def get_offset_target_section(sections, target_section, target_virtual_addr):
 
 def get_physical_start(sections, offset_target_section, target_section):
     physical_section_start = sections[target_section][2]
-    print(target_section)
     return hex(int(physical_section_start, 16) + int(offset_target_section, 16))
 
 
 
 if __name__ == "__main__":
-    # Get the total number of args passed to the demo.py
     vapid_args = list(sys.argv)
 
     if not valid_args(vapid_args):
         exit(1)
 
     exe_path = vapid_args[1]
+    
     target_virtual_addr = vapid_args[2]
+    
+    if not (vapid_args[2][:2] == '0x'):
+        target_virtual_addr = (hex(int(target_virtual_addr)))
+
     pe = pefile.PE(exe_path)
 
     if not check_32bit(pe):
         print("{} must be a 32-bit .exe")
 
-    addr_of_entry_point = get_entry_point(pe)   # Get entry point
+    
     image_base = get_image_base(pe)     # Get image base
+    
     sections = get_sections(pe, image_base)     # Create dictionary of sections in the format 'key:[rva, va, physical]'
+    
     target_section = get_target_section(sections, target_virtual_addr)     # Get target section and address
+    
     offset_target_section = get_offset_target_section(sections, target_section, target_virtual_addr)    # Get offset into the target section
+    
     rva_physical = get_physical_start(sections, offset_target_section, target_section)      # Add offset to the start of the section on disk
-    print("Physical Location: {}".format(rva_physical))
+    
+    print("{} -> {}".format(target_virtual_addr, rva_physical))
+    
 else:
     print("Whoops")
     exit(1)
